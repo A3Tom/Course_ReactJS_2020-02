e61c9594a8cef65a27214a257baf07f806e11050
A3Tom <Tom_Esq@Yahoo.com>
2021-01-04 18:33:34 +0000

    Refactored app.js to now handle lists of books

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import React, { Component } from "react";
 
 export default class App extends Component {
 	render() {
-		return (
-			<section className="booklist">
-				<Book imgSrc={firstBook.imgSrc} title={firstBook.title} author={firstBook.author} />
-				<Book imgSrc={secondBook.imgSrc} title={secondBook.title} author={secondBook.author} />
-			</section>
-		);
+		return <section className="booklist">{bookList}</section>;
 	}
 }
 
-const Book = ({ imgSrc, title, author }) => {
+const Book = (props) => {
+	const { imgSrc, title, author } = props;
 	return (
 		<article className="book">
 			<img src={imgSrc} alt="" />
@@ -21,14 +17,27 @@ const Book = ({ imgSrc, title, author }) => {
 	);
 };
 
-const firstBook = {
-	imgSrc: "https://images-eu.ssl-images-amazon.com/images/I/613iawXyW6L._AC_UL200_SR200,200_.jpg",
-	title: "Greenlights",
-	author: "Matthew McConaughey",
-};
+const books = [
+	{
+		id: 1,
+		imgSrc: "https://images-eu.ssl-images-amazon.com/images/I/613iawXyW6L._AC_UL200_SR200,200_.jpg",
+		title: "Greenlights",
+		author: "Matthew McConaughey",
+	},
+	{
+		id: 2,
+		imgSrc: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
+		title: "Atomic Habits",
+		author: "James Clear",
+	},
+	{
+		id: 3,
+		imgSrc: "https://images-eu.ssl-images-amazon.com/images/I/81LrxW8HNJL._AC_UL200_SR200,200_.jpg",
+		title: "Ottolenghi Flavour",
+		author: "Yotam Ottolenghi",
+	},
+];
 
-const secondBook = {
-	imgSrc: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
-	title: "Atomic Habits",
-	author: "James Clear",
-};
+const bookList = books.map((book) => {
+	return <Book key={book.id} {...book} />;
+});
